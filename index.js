916d86721eefe0c8dd2ed0d7790b4cc6c6954b55
shacharmalka1 <shacharmalka0204@gmail.com>
2021-10-17 00:41:07 +0300

    changes
    bug fixed

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ const getData = async (nameOrId) => {
     removePreviousTypesFromDom(".newType");
     removePreviousTypesFromDom(".new-poke-by-type");
     const resPokemon = await axios.get(`${url}/pokemon/${nameOrId}`);
-    console.log(resPokemon.data);
     document.getElementById("img").src = resPokemon.data.sprites.front_default;
     document.getElementById("name-value").textContent = resPokemon.data.name;
     document.getElementById("height-value").textContent =
@@ -30,13 +29,14 @@ const getData = async (nameOrId) => {
       newSpan.textContent = `| ${resPokemon.data.types[i].type.name} | `;
       document.getElementById("types-value").append(newSpan);
     }
-    document.getElementById("img").addEventListener("mouseover", () => {
-      document.getElementById("img").src = resPokemon.data.sprites.back_default;
-    });
-    document.getElementById("img").addEventListener("mouseout", () => {
-      document.getElementById("img").src =
-        resPokemon.data.sprites.front_default;
-    });
+    img.src = resPokemon.data.sprites.front_default;
+    img.onmouseenter = function () {
+      img.src = resPokemon.data.sprites.back_default;
+    };
+    img.onmouseout = function () {
+      img.src = resPokemon.data.sprites.front_default;
+    };
+
     getType();
   } catch (error) {
     alert("error");
@@ -52,14 +52,13 @@ function getType() {
         .replaceAll("|", "")
         .replaceAll(" ", "");
       const resType = await axios.get(`${url}/type/${typeText}`);
-      console.log(resType.data.pokemon);
       showAllRelatedPokemonsOnDom(resType.data.pokemon);
       document
         .getElementById("related-pokemons")
         .addEventListener("click", (e) => {
           e.stopImmediatePropagation();
-          console.log("time ");
           getData(e.target.textContent);
+          userText.value = e.target.textContent;
         });
     });
 }
