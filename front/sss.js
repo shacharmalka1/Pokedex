ff0d70d85a8decbe90f017bd007f326ac8eb7287
shacharmalka1 <shacharmalka0204@gmail.com>
2021-11-02 01:09:49 +0200

    .

---
+++ front/sss.js
@@ -0,0 +1,22 @@
+class Animal {
+
+    constructor(name) {
+      this.name = name;
+    }
+  
+  }
+  
+  class Rabbit extends Animal {
+    constructor(name) {
+      super(name);
+      this.created = Date.now();
+    }
+  }
+  
+  let rabbit = new Rabbit("White Rabbit"); 
+  console.log(rabbit.name);
+
+  let obj = {name:"sss"};
+
+console.log(obj.__proto__ === Object.prototype);
+console.log(obj.__proto__);
